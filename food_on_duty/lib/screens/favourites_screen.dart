d17d0ba4b2d3467e6d057f2dea64d69e801ee405
Akash Nadadur <nsakashu@gmail.com>
2020-04-12 20:34:57 +0530

    Newly changed

--- food_on_duty/lib/screens/favourites_screen.dart
+++ food_on_duty/lib/screens/favourites_screen.dart
@@ -2,19 +2,13 @@ import 'package:flutter/material.dart';
 import '../widgets/meal_item.dart';
 import '../models/meal.dart';
 
-class FavouritesScreen extends StatefulWidget {
+class FavouritesScreen extends StatelessWidget {
 
   final List<Meal> favouriteMeals;
   FavouritesScreen(this.favouriteMeals);
-
-  @override
-  _FavouritesScreenState createState() => _FavouritesScreenState();
-}
-
-class _FavouritesScreenState extends State<FavouritesScreen> {
   @override
   Widget build(BuildContext context) {
-    if(widget.favouriteMeals.isEmpty) {
+    if(favouriteMeals.isEmpty) {
       return Center(child: Text("No Favoutites"),);
     }
     else {
@@ -25,16 +19,16 @@ class _FavouritesScreenState extends State<FavouritesScreen> {
                 // decoration:
                 //     BoxDecoration(borderRadius: BorderRadius.circular(30)),
                 child: MealItem(
-                  id: widget.favouriteMeals[index].id,
-                  title: widget.favouriteMeals[index].title,
-                  duration: widget.favouriteMeals[index].duration,
-                  imageUrl: widget.favouriteMeals[index].imageUrl,
-                  complexity: widget.favouriteMeals[index].complexity,
-                  affordability: widget.favouriteMeals[index].affordability,
+                  id: favouriteMeals[index].id,
+                  title: favouriteMeals[index].title,
+                  duration: favouriteMeals[index].duration,
+                  imageUrl: favouriteMeals[index].imageUrl,
+                  complexity: favouriteMeals[index].complexity,
+                  affordability: favouriteMeals[index].affordability,
                   // removeItem: _removeMeal,
                   ));
           },
-          itemCount: widget.favouriteMeals.length,
+          itemCount: favouriteMeals.length,
         );
   }
   }
